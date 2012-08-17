952495166d41f5591ad9d89b9feba7202c90a778
Nelson <nelson211145@gmail.com>
2012-08-17 22:37:09 +0800

    initial of nelsonisapple

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nelsonisapple::Application.config.secret_token = '68f3c2fb0945d80002d82a85afe657477cab349f2670bff09671b674069e0906e4c02b47ba81189346d9aff428bb2531c092dfad4f8b85702f703d17cf7f33ad'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Nelsonisapple::Application.config.session_store :cookie_store, key: '_nelsonisapple_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Nelsonisapple::Application.config.session_store :active_record_store
